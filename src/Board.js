ca5b43bb1f4e2d65eb55f3a5e989a764b5641d9b
briannayoo <briannayoo94@gmail.com>
2024-06-10 11:47:24 +0900

    first commit

---
+++ src/Board.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Board extends Component {
+  render() {
+    let date = this.props.data.update_date !== '000-00-00'? this.props.data.update_date : this.props.data.date;
+    
+    return (
+      <tr>              
+        <th scope="row">
+        <input 
+          type="checkbox"          
+          onChange={(e)=>{
+            this.props.onCheckboxChange(
+              e.target.checked,
+              e.target.value
+            )}
+          } 
+          value={this.props.data.id}
+        />
+        </th>
+        <td>{this.props.data.id}</td>
+        <td>
+          <Link to={`/view?id=${this.props.data.id}`}>
+            {this.props.data.title}
+          </Link>
+          </td>
+        <td>{this.props.data.user_id}</td>
+        <td>{this.props.data.update_date}</td>
+      </tr>
+    )
+  }
+}
---
+++ src/view.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
+import { Link, useLocation } from 'react-router-dom';
+
+function View(){
+  let location = useLocation();
+  const id = new URLSearchParams(location.search).get(id);
+  const[form, setForm] = useState({
+    title: '',
+    content:''
+  })
+  
+
+  let detail = () =>{
+    Axios.get(`34.64.62.209:8000/detail?id=${id}`)
+    .then( res => {
+      if(res.data.length > 0){
+        setForm({
+          title:res.data[0].title,
+          content:res.data[0].content,
+        })
+      }
+    })
+    .catch(function (error) {     
+      console.log(error);
+    });
+  }
+  useEffect(()=>{
+    detail();
+  })
+  return (
+  <div>
+    <h2>글 상세</h2>
+    <h3>{form.title}</h3>
+    <div>{form.content}</div>
+    <hr/>
+    <Link to='/'>
+
+    </Link>
+  </div>
+)
+}
+
+export default View;
